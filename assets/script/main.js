3277604cf01b0657235c5ba243c53c6d8503921f
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2026-01-09 15:15:47 +0100

    Add destinations tabs crossfade effect

--- assets/script/main.js
+++ assets/script/main.js
@@ -13,37 +13,29 @@ const currentPage = window.location.pathname.split("/").pop() || "index.html";
 const navLinks = document.querySelectorAll(".main-nav-item a");
 
 navLinks.forEach(link => {
-    const linkPage = link.getAttribute("href");
+  const linkPage = link.getAttribute("href");
 
-    if (linkPage === currentPage) {
-        link.closest("li").classList.add("current");
-    };
+  if (linkPage === currentPage) {
+    link.closest("li").classList.add("current");
+  };
 });
 
 // Tabs
 
 function openTab(tabNumber) {
-    !tabNumber ? tabNumber = 1 : tabNumber;
+  !tabNumber ? tabNumber = 1 : tabNumber;
 
-    const tabImages = document.querySelectorAll(".tab-image");
-    const tabTexts = document.querySelectorAll(".tab-text");
-    const tabButtons = document.querySelectorAll(".tab-button");
-    
-    tabImages.forEach(tabImage => {
-      tabImage.style.display = "none";
-    });
+  const tabImages = document.querySelectorAll(".tab-image");
+  const tabTexts = document.querySelectorAll(".tab-text");
+  const tabButtons = document.querySelectorAll(".tab-button");
 
-    tabTexts.forEach(tabText => {
-      tabText.style.display = "none";
-    });
+  tabImages.forEach(img => img.classList.remove("active"));
+  tabTexts.forEach(txt => txt.classList.remove("active"));
+  tabButtons.forEach(btn => btn.classList.remove("active"));
 
-    tabButtons.forEach(tabButton => {
-      tabButton.classList.remove("active");
-    });
-
-    document.getElementById(`tab-image-${tabNumber}`).style.display = "flex";
-    document.getElementById(`tab-text-${tabNumber}`).style.display = "block";
-    document.getElementById(`tab-button-${tabNumber}`).classList.add("active");
+  document.getElementById(`tab-image-${tabNumber}`).classList.add("active");
+  document.getElementById(`tab-text-${tabNumber}`).classList.add("active");
+  document.getElementById(`tab-button-${tabNumber}`).classList.add("active");
 }
 
 openTab();
